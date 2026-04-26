e83a3b3cd1dac57303af2f006bf488ab4e345adf
Mubashir Aziz <cs.mubashir.a@gmail.com>
2026-04-26 16:59:50 +0500

    Use the pipeline in the main.py file

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from zeusoptimizedsdpipeline!")
+import torch
+from torchvision.utils import save_image
 
+from zeus_sd_pipeline.zeus_pipeline import ZeusOptimizedStableDiffusionPipeline
 
-if __name__ == "__main__":
-    main()
+pipe = ZeusOptimizedStableDiffusionPipeline.from_pretrained(
+"stable-diffusion-v1-5/stable-diffusion-v1-5", torch_dtype=torch.float16
+)
+pipe = pipe.to("cuda")
+
+prompt = "a photo of an astronaut riding a horse on mars"
+image = pipe(prompt).images[0]
+save_image(image, "output.png")
